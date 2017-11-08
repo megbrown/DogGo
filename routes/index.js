5706bdf939077cf2642783ddf617a36d2c8b5c8b
Megan B <brown.m.j@outlook.com>
2017-11-08 09:27:50 -0600

    pivot to small project

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/');
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.use(require('./select'));
+router.use(require('./rides'));
+router.use(require('./shelters'));
+router.use(require('./dogs'));
 
 module.exports = router;
