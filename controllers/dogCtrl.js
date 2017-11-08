dbdbf934ee5ec0f36c4f42c0af68d62c82528fad
Megan B <brown.m.j@outlook.com>
2017-11-08 11:35:38 -0600

    gets for dogs

--- controllers/dogCtrl.js
+++ controllers/dogCtrl.js
@@ -1,15 +1,44 @@
 'use strict';
 
 module.exports.getSelectedDogs = (req, res, next) => {
-	res.render('select-dogs');
+	const { Dog } = req.app.get('models');
+	Dog
+		.findAll({
+			where: { status: "selected" }
+		})
+		.then(results => {
+			res.json(results);
+			// res.render('select-dogs');
+		})
+		.catch(err => {
+			next(err);
+		})
 };
 
 module.exports.getAllDogs = (req, res, next) => {
-	res.render('all-dogs');
+	const { Dog } = req.app.get('models');
+	Dog
+		.findAll()
+		.then(results => {
+			// res.json(results);
+			res.render('all-dogs');
+		})
+		.catch(err => {
+			next(err);
+		})
 };
 
 module.exports.getSingleDog = (req, res, next) => {
-	res.render('single-dog');
+	const { Dog } = req.app.get('models');
+	Dog
+		.findById(req.params.id)
+		.then(results => {
+			res.json(results);
+			// res.render('single-dog');
+		})
+		.catch(err => {
+			next(err);
+		})
 };
 
 module.exports.updateDogStatus = (req, res, next) => {
